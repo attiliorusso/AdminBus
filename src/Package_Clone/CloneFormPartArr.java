4276596e5b9603a0ad12c7b41b76f0d581ecd44a
submarine91 <attilio.russo91@gmail.com>
2019-03-17 23:27:30 +0100

    Add a new class of clone.

---
+++ src/Package_Clone/CloneFormPartArr.java
@@ -0,0 +1,62 @@
+package Package_Clone;
+
+public class CloneFormPartArr {
+	public CloneFormPartArr() {
+		frame = new JFrame();
+		frame.setTitle("Admin bus App");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new BorderLayout());
+		
+		JPanel panel1 = new JPanel();
+		panel1.setLayout(new GridLayout(3, 2));
+		JLabel lab1 = new JLabel("Partenza (From): ");
+		panel1.add(lab1);
+		List<Località> l1 = DAOFactory.getDAOLocalità().doRetrieveAll();
+		JComboBox<Località> c1 = new JComboBox<>();
+		for(Località l:l1)
+			c1.addItem(l);
+		panel1.add(c1);
+		JLabel lab2 = new JLabel("Arrivo (To): ");
+		panel1.add(lab2);
+		List<Località> l2 = DAOFactory.getDAOLocalità().doRetrieveAll();
+		JComboBox<Località> c2 = new JComboBox<>();
+		for(Località l:l2)
+			c2.addItem(l);
+		panel1.add(c2);
+		JButton btn_cerca = new JButton("Elenca corse");
+		JPanel panel2 = new JPanel();
+		panel2.add(btn_cerca);
+		
+		frame.add(panel1, BorderLayout.CENTER);
+		frame.add(panel2, BorderLayout.SOUTH);
+		
+		
+		ActionListener listener = new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				controller = SportelloSystem.getInstance();
+				if(c1.getSelectedItem().toString().equals(c2.getSelectedItem().toString())) {
+					frame.setVisible(true);
+					JOptionPane.showMessageDialog(null, "Si scelga una destinazione diversa dalla partenza");
+				}
+				else {
+					List<Corsa> listaCorse = controller.elencaCorse(c1.getSelectedItem().toString(), c2.getSelectedItem().toString());
+					frame.setVisible(false);
+					if(listaCorse.size() == 0) {
+						frame.setVisible(true);
+						JOptionPane.showMessageDialog(null, "Nessuna corsa disponibile!");
+					}
+					else
+						new CorseView(listaCorse);
+				}
+				
+			}
+		};
+		
+		btn_cerca.addActionListener(listener);
+		
+		frame.setLocation(700, 400);
+		frame.pack();
+		
+		frame.setVisible(true);
+	}
+}
